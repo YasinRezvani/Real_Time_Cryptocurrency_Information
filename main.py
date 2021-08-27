f794934d2a121c8be4cb5a73da98f2523c49c0b0
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-27 07:04:32 +0430

    set table and localtime and url to requests to api to give json

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import requests
 import time
 from beautifultable import BeautifulTable
+
+table = BeautifulTable()
+localtime = time.asctime(time.localtime(time.time()))
+print("\n--- "+localtime+" ---")
+url = "https://api.coinlore.net/api/tickers/?start=0&limit=11"
+res = requests.get(url).json()
